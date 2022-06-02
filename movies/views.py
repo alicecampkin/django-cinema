ada95fe386b32e14d279725b77862c1f0e87da17
Alice Campkin <alice.campkin@gmail.com>
2022-06-02 21:24:56 +0100

    adds movie list

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.db.models.functions import Concat
+from django.db.models import F, Value
+from .models import Movie
 
 # Create your views here.
+
+
+def list_movies(request):
+
+    movies = Movie.objects.all().order_by("-popularity").annotate(
+        thumbnail_url=Concat(
+            Value("https://image.tmdb.org/t/p/w500"), F("poster_path"))
+    )
+
+    featured_movie = movies.first()
+
+    context = {
+        "featured_movie": featured_movie,
+        "featured_movie_image": f"https://image.tmdb.org/t/p/original{featured_movie.backdrop_path}",
+        "movies": movies[1:]
+    }
+
+    return render(request, "list_movies.html", context)
